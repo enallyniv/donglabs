4d94f382883207c1a81306a058f0f4caaa80ecd9
Vince Lane <enallyniv@gmail.com>
2015-12-12 01:06:54 +0000

    single-post viewing and a bit of renaming across the board

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     url(r'^$', views.post_list_main),
     url(r'^blog/$', views.post_list_main, name='front-page'),
     url(r'^blog/([0-9]+)/$', views.post_list, name='post-list'),
+    url(r'^blog/post/([0-9]+)/$', views.view_single, name='view-single'),
     url(r'^blog/about/$', views.about, name='about-page'),
-    url(r'^blog/post/$', views.submit_post, name='post-new'),
+    url(r'^blog/submit/$', views.submit_post, name='submit-post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from blog.models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-# For the main view just shit out the first page because what else would you show?
+# For the main view just shit out the first page of the post list.
 
 def post_list_main(request):
 
     return post_list(request, 1)
 
-# Try a paginated post list
+# Main page post list.
 
 def post_list(request, pagenumber):
     all_posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date') # Select everything...
-    paginator = Paginator(all_posts, 3) # Show that many per page
+    paginator = Paginator(all_posts, 5) # Show that many per page
 
     try:
         posts = paginator.page(pagenumber)
@@ -26,11 +26,18 @@ def post_list(request, pagenumber):
         
     return render(request, 'blog/base_blog.html', {'posts': posts, 'paginator': paginator })
 
+# Single-post view, for viewing single posts!
+
+def view_single(request, postnumber):
+    singlepost = get_object_or_404(Post, pk=postnumber)
+    
+    return render(request, 'blog/base_blog_single.html', {'singlepost': singlepost, })
+ 
 
 # Post page
 
 def submit_post(request):
-    return render(request, 'blog/base_post.html', {})
+    return render(request, 'blog/base_submit.html', {})
 
 
 # About page
